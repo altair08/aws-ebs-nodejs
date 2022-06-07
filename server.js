67bd747f543e140ba17b6a05ffcffe55ad04ea53
altair08 <altairthinesh2@gmail.com>
2022-06-07 15:14:02 +0530

    nodejs

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => res.json("API Running"));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
